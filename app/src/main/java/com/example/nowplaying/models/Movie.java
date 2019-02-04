5aefc3c87bd9e0cc1694c375aebdac2321302701
Joshua Alpert <joshua.alpert1@gmail.com>
2019-02-04 13:22:38 -0800

    getPosterPath() now appends the poster path to the base url before returning

--- app/src/main/java/com/example/nowplaying/models/Movie.java
+++ app/src/main/java/com/example/nowplaying/models/Movie.java
@@ -31,7 +31,7 @@ public class Movie {
     }
 
     public String getPosterPath() {
-        return posterPath;
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath) ;
     }
 
     public String getTitle() {
