1102d771a4da037e5ef68c03d5aa2d88dc3de87d
Joshua Alpert <joshua.alpert1@gmail.com>
2019-02-04 16:44:08 -0800

    Working on implementing the recylcer adapter for Movie

---
+++ app/src/main/java/com/example/nowplaying/adapters/MoviesAdapter.java
@@ -0,0 +1,65 @@
+package com.example.nowplaying.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.nowplaying.R;
+import com.example.nowplaying.models.Movie;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder>{
+
+    Context context;
+    List<Movie> movies;
+
+    public MoviesAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Movie movie = movies.get(position);
+        //Put the movie data into the view holder
+        viewHolder.bind(movie);
+    }
+
+    //Telling the view how big the data size is
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie){
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+        }
+    }
+}
