974b31fbe9c05b79cf83ae5cea8265cbf770fcc3
Joshua Alpert <joshua.alpert1@gmail.com>
2019-02-07 16:49:45 -0800

    Added stuff to activity_detail.xml

--- app/src/main/java/com/example/nowplaying/models/DetailActivity.java
+++ app/src/main/java/com/example/nowplaying/models/DetailActivity.java
@@ -11,5 +11,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        String title = getIntent().getStringExtra("title");
     }
 }
